3a8973f489612b53d8f7e2b27385ace07f8e5556
abelph <abelp76@gmail.com>
2021-10-06 12:16:32 -0500

    Added script to look through JSON file and get number of comments into a readable list.

--- json_reader.py
+++ json_reader.py
@@ -1,4 +1,5 @@
 import json
+import praw
 
 comments = []
 i = 0
@@ -8,4 +9,14 @@ for line in open("C:/Users/abelp/Documents/Classes/Networks/Data/RS_politics_201
     if i >= 10000:
         break
 
-print(comments[100])
+num = 10
+print(comments[num])
+
+reddit = praw.Reddit(
+    user_agent="Comment Extraction",
+    client_id="etDodny8ugn-bhb5NQ20Vw",
+    client_secret="BCD96zi9uth7HDNG7gqKvbqw13mjAg",
+)
+
+comment = reddit.comment(id=str(comments[num]['id']))
+#print(comment.parent())
