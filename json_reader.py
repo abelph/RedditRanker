c21be642ac5b696a206156589d7591a891789d95
abelph <abelp76@gmail.com>
2021-10-06 16:24:43 -0500

    Updated to gather only submissions from r/politics.

--- json_reader.py
+++ json_reader.py
@@ -1,16 +1,19 @@
 import json
 import praw
 
-comments = []
+approvedSubreddits = ['t5_2cneq']
+submissions = []
 i = 0
 for line in open("C:/Users/abelp/Documents/Classes/Networks/Data/RS_2019-09", 'r'):
-    comments.append(json.loads(line))
-    i += 1
-    if i >= 10000:
-        break
+    j = json.loads(line)
+    if j['subreddit_id'] in approvedSubreddits:
+        submissions.append(j)
+        i += 1
+        if i >= 10000:
+            break
 
-num = 160
-print(comments[num])
+num = 69
+print(submissions[num])
 
 reddit = praw.Reddit(
     user_agent="Comment Extraction",
@@ -18,7 +21,7 @@ reddit = praw.Reddit(
     client_secret="BCD96zi9uth7HDNG7gqKvbqw13mjAg",
 )
 
-submission = reddit.submission(id = str(comments[num]['id']))
+submission = reddit.submission(id = str(submissions[num]['id']))
 #comment = reddit.comment(id='hfl96kv')
 print(submission.subreddit)
-print(submission.comments.list())
+print(submission.comments[2].body)
