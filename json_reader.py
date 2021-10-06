b93b363834047098480dfb4f5509232744e64edd
abelph <abelp76@gmail.com>
2021-10-06 15:45:11 -0500

    Updated gitignore file

--- json_reader.py
+++ json_reader.py
@@ -3,13 +3,13 @@ import praw
 
 comments = []
 i = 0
-for line in open("C:/Users/abelp/Documents/Classes/Networks/Data/RS_politics_2016/RS_politics_2016.json", 'r'):
+for line in open("C:/Users/abelp/Documents/Classes/Networks/Data/RS_2019-09", 'r'):
     comments.append(json.loads(line))
     i += 1
     if i >= 10000:
         break
 
-num = 10
+num = 160
 print(comments[num])
 
 reddit = praw.Reddit(
@@ -18,5 +18,7 @@ reddit = praw.Reddit(
     client_secret="BCD96zi9uth7HDNG7gqKvbqw13mjAg",
 )
 
-comment = reddit.comment(id=str(comments[num]['id']))
-#print(comment.parent())
+submission = reddit.submission(id = str(comments[num]['id']))
+#comment = reddit.comment(id='hfl96kv')
+print(submission.subreddit)
+print(submission.comments.list())
