4fb9d97ba468c568fa4a4459b656c4d3b41e1fcc
Abel <abelp76@gmail.com>
2021-11-30 02:19:42 -0600

    Added additional subreddits to list

--- zst_extract.py
+++ zst_extract.py
@@ -1,5 +1,5 @@
 import zstandard
 
 dctx = zstandard.ZstdDecompressor(max_window_size=2147483648)
-with open(r"C:\Users\Abel\Documents\Classes\Networks\Data\RS_2018-07.zst", 'rb') as ifh, open(r"C:\Users\Abel\Documents\Classes\Networks\Data\RS_2018-07", 'wb') as ofh:
+with open(r"C:\Users\Abel\Documents\Classes\Networks\Data\RS_2020-11.zst", 'rb') as ifh, open(r"C:\Users\Abel\Documents\Classes\Networks\Data\RS_2020-11", 'wb') as ofh:
     dctx.copy_stream(ifh, ofh)
