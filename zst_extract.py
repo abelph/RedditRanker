beae0bac3139e2fda4d771b8319bfa7cd1d47893
Abel <abelp76@gmail.com>
2021-12-06 17:03:06 -0600

    Added comments

--- zst_extract.py
+++ zst_extract.py
@@ -1,6 +1,7 @@
 import zstandard
 
 dctx = zstandard.ZstdDecompressor(max_window_size=2147483648)
+# Change to paths where data ZST file is located and output should be stored
 with open(r"C:\Users\abelp\Documents\Classes\Networks\Data\RS_2020-11.zst", 'rb') as ifh, \
         open(r"C:\Users\abelp\Documents\Classes\Networks\Data\RS_2020-11", 'wb') as ofh:
     dctx.copy_stream(ifh, ofh)
